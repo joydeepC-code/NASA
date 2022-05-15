53e498d0fe2fc7c91eb443344fe87d943feffbe2
joydeepC-code <joydeepchanda1986@gmail.com>
2022-05-15 16:45:56 +0530

    animations

--- script.js
+++ script.js
@@ -1,2 +1,55 @@
+gsap.from("#background", {
+    duration: 2,
+    ease: "expo.inOut",
+    scale: 2.5,
+    opacity: 0
+});
 
 
+gsap.from("nav h3", {
+    duration: 3.5,
+    ease: "expo.inOut",
+    x: -300
+});
+
+
+gsap.from("nav a", {
+    duration: 3.5,
+    ease: "expo.inOut",
+    x: 300
+});
+
+gsap.from("#background h1", {
+    duration: 3,
+    ease: "expo.inOut",
+    opacity: 0,
+    y: -100
+});
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#content",
+        start: "top",
+        toggleActions: 'play pause resume reverse'
+    }
+});
+
+tl2.from('.block', {
+    stagger: .3,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+})
+
+.from('.block .img', {
+    stagger: .4,
+    opacity: 0,
+    duration: 1.5,
+})
+
+.from('footer p', {
+    stagger: .5,
+    y: 10,
+    opacity: 0,
+    duration: 3,
+})
